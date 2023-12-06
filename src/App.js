00c6ddc34b6b386ef5143826125756d6276244c3
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2023-12-06 14:29:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    {/* Navigation bar */}
-      <div>
-        <navbar>
-          <div class ="nav-cover" style={{backgroundColor:'green'}}>
-            <ul class="nav nav-head">
-              <li class="nav-item">Jolly Patel</li>
-            </ul>
-            <ul class="nav nav-side">
-              <li class="nav-item">Work</li>
-              <li class="nav-item">Skills</li>
-              <li class="nav-item">Education</li>
-              <li class="nav-item">About</li>
-            </ul>
-          </div>
-        </navbar>
-      </div>
-      {/* Navigation bar ends */}
+    
     </div>
   );
 }
