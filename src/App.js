88a08de3b15f94a5cb91ce88e17de327c787f589
JollyPatel25 <JollyPatel25@github.com>
2023-12-12 11:03:21 +0530

    Nav

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    {/* Navigation bar */}
-      <div>
-        <navbar>
-          <div class ="nav-cover" style={{backgroundColor:'green'}}>
-            <ul class="nav nav-head">
-              <li class="nav-item">Jolly Patel</li>
-            </ul>
-            <ul class="nav nav-side">
-              <li class="nav-item">Work</li>
-              <li class="nav-item">Skills</li>
-              <li class="nav-item">Education</li>
-              <li class="nav-item">About</li>
-            </ul>
-          </div>
-        </navbar>
-      </div>
+      {/* Navigation bar */}
+      <navbar>
+        <div class="nav-cover" style={{ backgroundColor: "pink" }}>
+          <ul class="nav">
+            <li class="nav-item nav-head text-hov">Jolly Patel</li>
+            <li class="nav-item nav-side text-hov">Work</li>
+            <li class="nav-item nav-side text-hov">Skills</li>
+            <li class="nav-item nav-side text-hov">Education</li>
+            <li class="nav-item nav-side text-hov">About</li>
+          </ul>
+        </div>
+      </navbar>
       {/* Navigation bar ends */}
     </div>
   );
