ab6f485ab1b25afcf4a63d6b1cb288435a36008b
Jolly Patel <jollypatel2504@gmail.com>
2024-05-19 10:04:08 +0530

    starting scratch

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 // App.js
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import NavigationBar from './Components/NavigationBar.js';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading time
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  useEffect(() => {
-    const imageElement = document.querySelector('.full-screen-image');
-    if (imageElement) {
-      if (isLoading) {
-        imageElement.classList.remove('show');
-      } else {
-        imageElement.addEventListener('transitionend', handleTransitionEnd);
-      }
-    }
-  }, [isLoading]);
-
-  const handleTransitionEnd = () => {
-    const imageElement = document.querySelector('.full-screen-image');
-    if (imageElement) {
-      imageElement.remove();
-    }
-  };
 
   return (
     <div className="app">
-      <div className={`loading-screen ${isLoading ? 'loading' : ''}`}>
-        <div className={`full-screen-image ${!isLoading ? 'show' : ''}`}></div>
-      </div>
-      <div className={`home-screen ${!isLoading ? 'show' : ''}`}>
-        <NavigationBar />
-      </div>
+      
     </div>
   );
 }
