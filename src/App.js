39c45bf46335bd9259a3d71b3e657d84678b3b74
Jolly Patel <jollypatel2504@gmail.com>
2024-06-14 17:39:45 +0530

    make navbar responsive

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./CSS/NavBar.css";
 function App() {
   return (
     <div className="app">
-      <div className="NavigationBar home-one">
+      <div className="NavigationBar">
         <ul className="my-name-head"><li>Jolly Patel</li></ul>
         <ul className='navbar'>
           <li>Experience</li>
@@ -17,21 +17,39 @@ function App() {
       <div className="home-two">
         <div className="home-two-canvas">
           <div className='inner-home-two-canvas'>
-            <table>
-              <tr>
-                <td> <div className="profile-pic"></div></td>
-                <td><div className="data"><p>Currently, I'm pursuing a Bachelor's degree in Computer Engineering, 
-              where I'm immersing myself in the ever-evolving world of technology. 
-              With a keen eye on emerging trends and a thirst for knowledge, I'm constantly seeking 
-              opportunities to broaden my skill set and contribute meaningfully to collaborative projects. 
-              As a passionate and forward-thinking developer, I'm driven by innovation and inspired by 
-              the potential of technology to shape the future.</p></div></td>
-              </tr>
-            </table>
+            <div className="profile-pic"></div>
+            <div className="data">
+              <p>Currently, I'm pursuing a Bachelor's degree in Computer Engineering, 
+                where I'm immersing myself in the ever-evolving world of technology. 
+                With a keen eye on emerging trends and a thirst for knowledge, I'm constantly seeking 
+                opportunities to broaden my skill set and contribute meaningfully to collaborative projects. 
+                As a passionate and forward-thinking developer, I'm driven by innovation and inspired by 
+                the potential of technology to shape the future.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="home-two">
+        <div className="home-two-canvas">
+          <div className='inner-home-two-canvas'>
+            <div className="profile-pic"></div>
+            <div className="data">
+              <p>Currently, I'm pursuing a Bachelor's degree in Computer Engineering, 
+                where I'm immersing myself in the ever-evolving world of technology. 
+                With a keen eye on emerging trends and a thirst for knowledge, I'm constantly seeking 
+                opportunities to broaden my skill set and contribute meaningfully to collaborative projects. 
+                As a passionate and forward-thinking developer, I'm driven by innovation and inspired by 
+                the potential of technology to shape the future.
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
 export default App;
