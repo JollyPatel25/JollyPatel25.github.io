915a780e4834a1a0bce236f37fb372f522a94244
Jolly Patel <jollypatel2504@gmail.com>
2024-06-14 17:40:43 +0530

    make navbar changes for responsiveness

--- src/App.js
+++ src/App.js
@@ -30,24 +30,6 @@ function App() {
           </div>
         </div>
       </div>
-
-
-      <div className="home-two">
-        <div className="home-two-canvas">
-          <div className='inner-home-two-canvas'>
-            <div className="profile-pic"></div>
-            <div className="data">
-              <p>Currently, I'm pursuing a Bachelor's degree in Computer Engineering, 
-                where I'm immersing myself in the ever-evolving world of technology. 
-                With a keen eye on emerging trends and a thirst for knowledge, I'm constantly seeking 
-                opportunities to broaden my skill set and contribute meaningfully to collaborative projects. 
-                As a passionate and forward-thinking developer, I'm driven by innovation and inspired by 
-                the potential of technology to shape the future.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
